e7964c5941246626d6fa4202c637e7f377ffe5a1
- <->
2023-10-26 11:08:45 +0700

    feat: add address data

--- server/constants.js
+++ server/constants.js
@@ -1,3 +1,5 @@
+import fs from "fs"
+
 export const DEFAULT_PAGE_SIZE = 10
 export const DEFAULT_PAGE_NUMBER = 1
 
@@ -9,4 +11,34 @@ export const STATUS_CODE_MESSAGE = {
   403: "Forbidden",
   404: "Not Found",
   500: "Internal Server Error",
+}
+
+let wards = []
+let districts = []
+let provinces = []
+
+const wardsText = fs.readFileSync("./data/wards.json", { encoding: "utf-8" })
+const districtsText = fs.readFileSync("./data/districts.json", {
+  encoding: "utf-8",
+})
+const provincesText = fs.readFileSync("./data/provinces.json", {
+  encoding: "utf-8",
+})
+
+try {
+  wards = JSON.parse(wardsText)
+} catch (error) {}
+
+try {
+  districts = JSON.parse(districtsText)
+} catch (error) {}
+
+try {
+  provinces = JSON.parse(provincesText)
+} catch (error) {}
+
+export const addresses = {
+  wards,
+  districts,
+  provinces,
 }
