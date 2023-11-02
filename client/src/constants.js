a3524fb7d9529faa57ee839130f8605afc6f8cda
cuongdotuan <dotuancuong911@gmail.com>
2023-11-02 09:22:32 +0700

    create default pageSize, pageNumber

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,8 @@
 export const USER = "USER"
 export const TOKEN = "TOKEN"
+export const DEFAULT_PAGINATION = {
+  PAGE_SIZE: 5,
+  PAGE_NUMBER: 1,
+}
+
+export const PAGE_SIZE_OPTIONS = [5, 10, 15, 20]
