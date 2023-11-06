9eb341c09d963cdfb4c1f6eb0f336e9fb968c69a
cuongdotuan <dotuancuong911@gmail.com>
2023-11-06 22:43:47 +0700

    controlled to uncontrolled in edit product page

--- client/src/pages/product/Edit.jsx
+++ client/src/pages/product/Edit.jsx
@@ -5,33 +5,51 @@ import { AppContext } from "../../context"
 import {
   Button,
   FormControl,
+  Input,
   InputLabel,
   MenuItem,
   Select,
   TextField,
 } from "@mui/material"
+import { Controller, useForm } from "react-hook-form"
 
 const EditProduct = () => {
-  const [detail, setDetail] = useState(null)
+  // const [detail, setDetail] = useState(null)
   const [categories, setCategories] = useState([])
   const [sizes, setSizes] = useState([])
   const data = useParams()
   const { setIsLoading, setSnackbar, setHeader } = useContext(AppContext)
   const navigate = useNavigate()
 
+  const { control, handleSubmit, reset } = useForm()
+
+  const onSubmit = (data) => console.log(data)
   useEffect(() => {
+    let ignore = false
     const getDetail = async () => {
-      const res = await api.get(`/products/${data.id}`)
-      setDetail(res.data)
+      try {
+        setIsLoading(true)
+        const res = await api.get(`/products/${data.id}`)
+        if (!ignore) {
+          reset(res.data)
+        }
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setIsLoading(false)
+      }
     }
     getDetail()
+    return () => {
+      ignore = true
+    }
   }, [])
 
   useEffect(() => {
-    setIsLoading(true)
     let ignore = false
     const getCategory = async () => {
       try {
+        setIsLoading(true)
         const respone = await api.get(`/categories`)
         if (!ignore) {
           setCategories(respone.data.items)
@@ -49,10 +67,10 @@ const EditProduct = () => {
   }, [])
 
   useEffect(() => {
-    setIsLoading(true)
     let ignore = false
     const getSizes = async () => {
       try {
+        setIsLoading(true)
         const respone = await api.get(`/sizes`)
         if (!ignore) {
           console.log(respone.data)
@@ -77,127 +95,131 @@ const EditProduct = () => {
     }
   }, [])
 
-  console.log(detail)
-  const handleChangeName = (e) => {
-    setDetail({ ...detail, name: e.target.value })
-  }
-  const handleChangeCategory = (e) => {
-    setDetail({ ...detail, categories: e.target.value })
-  }
-
-  const handleChangePrice = (e) => {
-    setDetail({ ...detail, price: e.target.value })
-  }
-  const handleChangeSize = (e) => {
-    setDetail({ ...detail, size: e.target.value })
-  }
-  const handleChangeColor = (e) => {
-    setDetail({ ...detail, color: e.target.value })
-  }
-  const handleChangeThumbnail = (e) => {
-    setDetail({ ...detail, thumbnail: e.target.value })
-  }
-  const onSubmit = () => {
-    setIsLoading(true)
-    const putUpdate = async () => {
-      try {
-        await api.put(`/products/${data.id}`, detail)
-        setSnackbar({
-          openSnackbar: true,
-          snackbarMessage: "Success",
-          snackbarSeverity: "success",
-        })
-        navigate(`/product`)
-      } catch (error) {
-        console.log(error)
-        setSnackbar({
-          openSnackbar: true,
-          snackbarMessage: "Error",
-          snackbarSeverity: "error",
-        })
-      } finally {
-        setIsLoading(false)
-      }
-    }
-    putUpdate()
-  }
+  // const onSubmit = () => {
+  //   setIsLoading(true)
+  //   const putUpdate = async () => {
+  //     try {
+  //       await api.put(`/products/${data.id}`, detail)
+  //       setSnackbar({
+  //         openSnackbar: true,
+  //         snackbarMessage: "Success",
+  //         snackbarSeverity: "success",
+  //       })
+  //       navigate(`/product`)
+  //     } catch (error) {
+  //       console.log(error)
+  //       setSnackbar({
+  //         openSnackbar: true,
+  //         snackbarMessage: "Error",
+  //         snackbarSeverity: "error",
+  //       })
+  //     } finally {
+  //       setIsLoading(false)
+  //     }
+  //   }
+  //   putUpdate()
+  // }
 
   return (
     <>
-      <div className="mx-auto p-4 shadow-xl flex flex-col gap-5">
-        <TextField
-          id="outlined-basic"
-          label="Name"
-          variant="outlined"
-          type="text"
-          className="w-full"
-          value={detail?.name || ""}
-          onChange={handleChangeName}
+      <form
+        className="flex flex-col gap-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Controller
+          name="name"
+          defaultValue={""}
+          control={control}
+          render={({ field }) => (
+            <TextField
+              id="outlined-basic"
+              label="Name"
+              variant="outlined"
+              type="text"
+              className="w-full"
+              {...field}
+            />
+          )}
         />
-
-        <TextField
-          id="outlined-basic"
-          label="Price"
-          variant="outlined"
-          type="number"
-          className="w-full"
-          value={detail?.price || ""}
-          onChange={handleChangePrice}
+        <Controller
+          name="description"
+          defaultValue={""}
+          control={control}
+          render={({ field }) => (
+            <TextField
+              id="outlined-multiline-static"
+              label="Multiline"
+              multiline
+              rows={4}
+              {...field}
+              className="w-full"
+            />
+          )}
         />
-        <FormControl
-          sx={{ m: 1, minWidth: 120 }}
-          className="m-0"
-        >
-          <InputLabel id="demo-select-small-label">Size</InputLabel>
-          <Select
-            labelId="demo-select-small-label"
-            id="demo-select-small"
-            value={detail?.size || ""}
-            label="Size"
-            onChange={handleChangeSize}
-          >
-            {sizes.map((s, idx) => (
-              <MenuItem
-                key={idx}
-                value={s}
-              >
-                {s}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <TextField
-          id="outlined-basic"
-          label="Color"
-          variant="outlined"
-          type="text"
-          className="w-full"
-          value={detail?.color || ""}
-          onChange={handleChangeColor}
+        <Controller
+          name="price"
+          defaultValue={""}
+          control={control}
+          render={({ field }) => (
+            <TextField
+              id="outlined-basic"
+              label="Price"
+              variant="outlined"
+              type="number"
+              className="w-full"
+              {...field}
+            />
+          )}
         />
-        <TextField
-          id="outlined-basic"
-          label="Thumbnail"
-          variant="outlined"
-          type="text"
-          className="w-full"
-          value={detail?.thumbnail || ""}
-          onChange={handleChangeThumbnail}
+        <Controller
+          name="color"
+          defaultValue={""}
+          control={control}
+          render={({ field }) => (
+            <TextField
+              id="outlined-basic"
+              label="Color"
+              variant="outlined"
+              type="text"
+              className="w-full"
+              {...field}
+            />
+          )}
         />
-
+        {/* <Controller
+          name="size"
+          control={control}
+          render={({ field }) => (
+            <Select
+              className="w-full"
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              label="Size"
+              {...field}
+            >
+              {sizes.map((s) => {
+                return (
+                  <MenuItem
+                    key={s}
+                    value={s}
+                  >
+                    {s}
+                  </MenuItem>
+                )
+              })}
+            </Select>
+          )}
+        /> */}
         <Button
           variant="contained"
-          className="w-full"
-          onClick={onSubmit}
+          type="submit"
+          className="w-2/5 mx-auto"
         >
-          change
+          Change
         </Button>
-      </div>
+      </form>
     </>
   )
 }
 
 export default EditProduct
-function setHeader(arg0) {
-  throw new Error("Function not implemented.")
-}
